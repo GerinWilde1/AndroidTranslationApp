36c1cd2cf4aaddc1f7010312b164c5fc823a990f
Gerin Wilde <91751688+GerinWilde1@users.noreply.github.com>
2024-11-22 15:00:36 -0700

    update

--- Translation_Panther/app/build.gradle.kts
+++ Translation_Panther/app/build.gradle.kts
@@ -1,23 +1,21 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
 }
 
 android {
     namespace = "com.example.translation_panther"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.translation_panther"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        vectorDrawables {
-            useSupportLibrary = true
-        }
     }
 
     buildTypes {
@@ -30,43 +28,35 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "11"
     }
     buildFeatures {
         compose = true
-    }
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.1"
-    }
-    packaging {
-        resources {
-            excludes += "/META-INF/{AL2.0,LGPL2.1}"
-        }
     }
 }
 
 dependencies {
 
-    
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.activity.compose)
-    implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.ui)
-    implementation(libs.androidx.ui.graphics)
-    implementation(libs.androidx.ui.tooling.preview)
-    implementation(libs.androidx.material3)
-    implementation ("com.google.mlkit:translate:17.0.3")
+    implementation(libs.androidx.core.ktx.v1120)
+    implementation(libs.androidx.appcompat.v161)
+    implementation(libs.material)
+    implementation(libs.androidx.constraintlayout.v214)
+    implementation(libs.androidx.library)
+    implementation(libs.androidx.ui.graphics.android)
+    implementation(libs.androidx.foundation.android)
+    implementation(libs.androidx.material3.android)
     testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.ui.test.junit4)
-    debugImplementation(libs.androidx.ui.tooling)
-    debugImplementation(libs.androidx.ui.test.manifest)
-    
+    androidTestImplementation(libs.androidx.junit.v115)
+    androidTestImplementation(libs.androidx.espresso.core.v351)
+    implementation(libs.play.services.mlkit.text.recognition.v1900)
+    implementation(libs.play.services.mlkit.text.recognition.chinese)
+    implementation(libs.play.services.mlkit.text.recognition.devanagari.v1600)
+    implementation(libs.play.services.mlkit.text.recognition.japanese.v1600)
+    implementation(libs.play.services.mlkit.text.recognition.korean.v1600)
+    implementation(libs.play.services.mlkit.language.id)
+    implementation(libs.translate.v1702)
 }
--- Translation_Panther/app/src/main/java/com/example/translation_panther/MainActivity.kt
+++ Translation_Panther/app/src/main/java/com/example/translation_panther/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.translation_panther
 
+
 import android.app.Activity
 import android.content.Intent
 import android.net.Uri
@@ -13,6 +14,7 @@ import com.google.mlkit.common.model.DownloadConditions
 import com.google.mlkit.nl.languageid.LanguageIdentification
 import com.google.mlkit.nl.translate.TranslateLanguage
 import com.google.mlkit.nl.translate.Translation
+import com.google.mlkit.nl.translate.Translator
 import com.google.mlkit.nl.translate.TranslatorOptions
 import com.google.mlkit.vision.common.InputImage
 import com.google.mlkit.vision.text.Text
@@ -29,16 +31,16 @@ import java.util.*
 class MainActivity : AppCompatActivity() {
 
     private lateinit var textToSpeech: TextToSpeech
-    private lateinit var progressBar: ProgressBar
-    private lateinit var etTranslate: EditText
-    private lateinit var tvResult: TextView
-    private lateinit var btnScan: Button
+
+    private lateinit var Translate: EditText
+    private lateinit var Result: TextView
+
     private lateinit var btnTranslate: Button
     private lateinit var btnRead: Button
-    private lateinit var imageView: ImageView
+
     private lateinit var fromSpinner: Spinner
-    private val PICK_IMAGE_REQUEST_CODE = 123
-    lateinit var recognizer: TextRecognizer
+
+
     val latinScript = Data.LatinScript
     val chineseLanguages = Data.chineseLanguages
     val devanagariLanguages = Data.devanagariLanguages
@@ -51,13 +53,11 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         // Initialize views
+        Translate = findViewById(R.id.Translator)
+        Result = findViewById(R.id.Result)
+        btnTranslate = findViewById(R.id.buttonTranslate)
+        btnRead = findViewById(R.id.buttonRead)
 
-        etTranslate = findViewById(R.id.etTranslate)
-        tvResult = findViewById(R.id.tvResult)
-        btnScan = findViewById(R.id.btnScan)
-        btnTranslate = findViewById(R.id.btnTranslate)
-        btnRead = findViewById(R.id.btnRead)
-        imageView = findViewById(R.id.image)
 
         val toSpinner: Spinner = findViewById(R.id.TOspinner)
         fromSpinner = findViewById(R.id.FROMSpinner)
@@ -77,7 +77,7 @@ class MainActivity : AppCompatActivity() {
             "JAPANESE", "GEORGIAN", "KANNADA", "KOREAN", "LITHUANIAN", "LATVIAN", "MACEDONIAN", "MARATHI", "MALAY",
             "MALTESE", "DUTCH", "NORWEGIAN", "POLISH", "PORTUGUESE", "ROMANIAN", "RUSSIAN", "SLOVAK", "SLOVENIAN",
             "ALBANIAN", "SWEDISH", "SWAHILI", "TAMIL", "TELUGU", "THAI", "TAGALOG", "TURKISH", "UKRAINIAN", "URDU",
-            "VIETNAMESE", "CHINESE", "others"
+            "VIETNAMESE", "CHINESE"
         )
         val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, options)
         fromSpinner.adapter = adapter
@@ -101,32 +101,176 @@ class MainActivity : AppCompatActivity() {
 
             override fun onNothingSelected(parent: AdapterView<*>?) {}
         }
+        fun translateText(from: String, to: String, text: String) {
 
-        btnScan.setOnClickListener {
-            etTranslate.text.clear()
-            if (fromLanguage != "others") {
-                setupRecognizer(fromLanguage)
-                openGallery()
-            } else {
-                Toast.makeText(this@MainActivity, "TO SCAN YOU HAVE TO CHOOSE A PARTICULAR LANGUAGE", Toast.LENGTH_LONG).show()
+            val f = when(from)
+            {
+                "ENGLISH" -> TranslateLanguage.ENGLISH
+                "GERMAN" -> TranslateLanguage.GERMAN
+                "HINDI" -> TranslateLanguage.HINDI
+                "MARATHI" -> TranslateLanguage.MARATHI
+                "AFRIKAANS" -> TranslateLanguage.AFRIKAANS
+                "ARABIC" -> TranslateLanguage.ARABIC
+                "BELARUSIAN" -> TranslateLanguage.BELARUSIAN
+                "BULGARIAN" -> TranslateLanguage.BULGARIAN
+                "BENGALI" -> TranslateLanguage.BENGALI
+                "CATALAN" -> TranslateLanguage.CATALAN
+                "CZECH" -> TranslateLanguage.CZECH
+                "WELSH" -> TranslateLanguage.WELSH
+                "DANISH" -> TranslateLanguage.DANISH
+                "GREEK" -> TranslateLanguage.GREEK
+                "ESPERANTO" -> TranslateLanguage.ESPERANTO
+                "SPANISH" -> TranslateLanguage.SPANISH
+                "ESTONIAN" -> TranslateLanguage.ESTONIAN
+                "PERSIAN" -> TranslateLanguage.PERSIAN
+                "FINNISH" -> TranslateLanguage.FINNISH
+                "FRENCH" -> TranslateLanguage.FRENCH
+                "IRISH" -> TranslateLanguage.IRISH
+                "GALICIAN" -> TranslateLanguage.GALICIAN
+                "GUJARATI" -> TranslateLanguage.GUJARATI
+                "HEBREW" -> TranslateLanguage.HEBREW
+                "CROATIAN" -> TranslateLanguage.CROATIAN
+                "HAITIAN" -> TranslateLanguage.HAITIAN_CREOLE
+                "HUNGARIAN" -> TranslateLanguage.HUNGARIAN
+                "INDONESIAN" -> TranslateLanguage.INDONESIAN
+                "ICELANDIC" -> TranslateLanguage.ICELANDIC
+                "ITALIAN" -> TranslateLanguage.ITALIAN
+                "JAPANESE" -> TranslateLanguage.JAPANESE
+                "GEORGIAN" -> TranslateLanguage.GEORGIAN
+                "KANNADA" -> TranslateLanguage.KANNADA
+                "KOREAN" -> TranslateLanguage.KOREAN
+                "LITHUANIAN" -> TranslateLanguage.LITHUANIAN
+                "LATVIAN" -> TranslateLanguage.LATVIAN
+                "MACEDONIAN" -> TranslateLanguage.MACEDONIAN
+                "MALAY" -> TranslateLanguage.MALAY
+                "MALTESE" -> TranslateLanguage.MALTESE
+                "DUTCH" -> TranslateLanguage.DUTCH
+                "NORWEGIAN" -> TranslateLanguage.NORWEGIAN
+                "POLISH" -> TranslateLanguage.POLISH
+                "PORTUGUESE" -> TranslateLanguage.PORTUGUESE
+                "ROMANIAN" -> TranslateLanguage.ROMANIAN
+                "RUSSIAN" -> TranslateLanguage.RUSSIAN
+                "SLOVAK" -> TranslateLanguage.SLOVAK
+                "SLOVENIAN" -> TranslateLanguage.SLOVENIAN
+                "ALBANIAN" -> TranslateLanguage.ALBANIAN
+                "SWEDISH" -> TranslateLanguage.SWEDISH
+                "SWAHILI" -> TranslateLanguage.SWAHILI
+                "TAMIL" -> TranslateLanguage.TAMIL
+                "TELUGU" -> TranslateLanguage.TELUGU
+                "THAI" -> TranslateLanguage.THAI
+                "TAGALOG" -> TranslateLanguage.TAGALOG
+                "TURKISH" -> TranslateLanguage.TURKISH
+                "UKRAINIAN" -> TranslateLanguage.UKRAINIAN
+                "URDU" -> TranslateLanguage.URDU
+                "VIETNAMESE" -> TranslateLanguage.VIETNAMESE
+                "CHINESE" -> TranslateLanguage.CHINESE
+                else -> {
+                    TranslateLanguage.ENGLISH
+                }
             }
+            val t = when (to) {
+                "ENGLISH" -> TranslateLanguage.ENGLISH
+                "GERMAN" -> TranslateLanguage.GERMAN
+                "HINDI" -> TranslateLanguage.HINDI
+                "MARATHI" -> TranslateLanguage.MARATHI
+                "AFRIKAANS" -> TranslateLanguage.AFRIKAANS
+                "ARABIC" -> TranslateLanguage.ARABIC
+                "BELARUSIAN" -> TranslateLanguage.BELARUSIAN
+                "BULGARIAN" -> TranslateLanguage.BULGARIAN
+                "BENGALI" -> TranslateLanguage.BENGALI
+                "CATALAN" -> TranslateLanguage.CATALAN
+                "CZECH" -> TranslateLanguage.CZECH
+                "WELSH" -> TranslateLanguage.WELSH
+                "DANISH" -> TranslateLanguage.DANISH
+                "GREEK" -> TranslateLanguage.GREEK
+                "ESPERANTO" -> TranslateLanguage.ESPERANTO
+                "SPANISH" -> TranslateLanguage.SPANISH
+                "ESTONIAN" -> TranslateLanguage.ESTONIAN
+                "PERSIAN" -> TranslateLanguage.PERSIAN
+                "FINNISH" -> TranslateLanguage.FINNISH
+                "FRENCH" -> TranslateLanguage.FRENCH
+                "IRISH" -> TranslateLanguage.IRISH
+                "GALICIAN" -> TranslateLanguage.GALICIAN
+                "GUJARATI" -> TranslateLanguage.GUJARATI
+                "HEBREW" -> TranslateLanguage.HEBREW
+                "CROATIAN" -> TranslateLanguage.CROATIAN
+                "HAITIAN" -> TranslateLanguage.HAITIAN_CREOLE
+                "HUNGARIAN" -> TranslateLanguage.HUNGARIAN
+                "INDONESIAN" -> TranslateLanguage.INDONESIAN
+                "ICELANDIC" -> TranslateLanguage.ICELANDIC
+                "ITALIAN" -> TranslateLanguage.ITALIAN
+                "JAPANESE" -> TranslateLanguage.JAPANESE
+                "GEORGIAN" -> TranslateLanguage.GEORGIAN
+                "KANNADA" -> TranslateLanguage.KANNADA
+                "KOREAN" -> TranslateLanguage.KOREAN
+                "LITHUANIAN" -> TranslateLanguage.LITHUANIAN
+                "LATVIAN" -> TranslateLanguage.LATVIAN
+                "MACEDONIAN" -> TranslateLanguage.MACEDONIAN
+                "MALAY" -> TranslateLanguage.MALAY
+                "MALTESE" -> TranslateLanguage.MALTESE
+                "DUTCH" -> TranslateLanguage.DUTCH
+                "NORWEGIAN" -> TranslateLanguage.NORWEGIAN
+                "POLISH" -> TranslateLanguage.POLISH
+                "PORTUGUESE" -> TranslateLanguage.PORTUGUESE
+                "ROMANIAN" -> TranslateLanguage.ROMANIAN
+                "RUSSIAN" -> TranslateLanguage.RUSSIAN
+                "SLOVAK" -> TranslateLanguage.SLOVAK
+                "SLOVENIAN" -> TranslateLanguage.SLOVENIAN
+                "ALBANIAN" -> TranslateLanguage.ALBANIAN
+                "SWEDISH" -> TranslateLanguage.SWEDISH
+                "SWAHILI" -> TranslateLanguage.SWAHILI
+                "TAMIL" -> TranslateLanguage.TAMIL
+                "TELUGU" -> TranslateLanguage.TELUGU
+                "THAI" -> TranslateLanguage.THAI
+                "TAGALOG" -> TranslateLanguage.TAGALOG
+                "TURKISH" -> TranslateLanguage.TURKISH
+                "UKRAINIAN" -> TranslateLanguage.UKRAINIAN
+                "URDU" -> TranslateLanguage.URDU
+                "VIETNAMESE" -> TranslateLanguage.VIETNAMESE
+                "CHINESE" -> TranslateLanguage.CHINESE
+                else -> TranslateLanguage.ENGLISH // Default language if not found
+            }
+
+
+            val options = TranslatorOptions.Builder()
+                .setSourceLanguage(f)
+                .setTargetLanguage(t)
+                .build()
+            val translator: Translator = Translation.getClient(options)
+            val conditions = DownloadConditions.Builder()
+                .requireWifi()
+                .build()
+
+
+
+            translator.downloadModelIfNeeded(conditions)
+                .addOnSuccessListener {
+                    translator.translate(text)
+                        .addOnSuccessListener { translatedText ->
+                            Result.text = translatedText
+                        }
+                        .addOnFailureListener { exception ->
+                            Toast.makeText(this, "Translation failed: ${exception.message}", Toast.LENGTH_SHORT).show()
+                        }
+                }
+                .addOnFailureListener { exception ->
+                    Toast.makeText(this, "Model download failed: ${exception.message}", Toast.LENGTH_SHORT).show()
+                }
         }
 
         btnTranslate.setOnClickListener {
 
-            val textToTranslate = etTranslate.text.toString()
+            val textToTranslate = Translate.text.toString()
             if (textToTranslate.isNotEmpty()) {
-                if (fromLanguage == "others") {
-
-                    translateText(fromLanguage, toLanguage, textToTranslate)
-                }
+                translateText(fromLanguage, toLanguage, textToTranslate)
+            }
             else {
                 Toast.makeText(this@MainActivity, "Please enter text to translate", Toast.LENGTH_SHORT).show()
             }
         }
 
         btnRead.setOnClickListener {
-            val text = tvResult.text.toString()
+            val text = Result.text.toString()
             if (text.isEmpty()) {
                 Toast.makeText(this@MainActivity, "No text to read", Toast.LENGTH_SHORT).show()
             } else {
@@ -137,5 +281,6 @@ class MainActivity : AppCompatActivity() {
 
 
 
-    // Rest of the utility functions remain unchanged
+
 }
+
